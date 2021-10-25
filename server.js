288b85a2f8a1b5bd2a6a4d6f500f70815a09fecd
hanbee <sonhanbi1002@gmail.com>
2021-10-26 00:34:09 +0900

    sequelize sync 설정하기

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(cors(corsOptions))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+const db = require("./app/models")
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and re-sync db.')
+})
+
 // simple route
 app.get("/", (req, res) => {
     res.json({message: "Welcome to bcheck application"})
