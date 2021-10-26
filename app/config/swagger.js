f8580112e1115f6d7ca6ffd0d7af7f73c6280f2d
hanbee <sonhanbi1002@gmail.com>
2021-10-26 13:07:51 +0900

    swagger config 설정

---
+++ app/config/swagger.js
@@ -0,0 +1,28 @@
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsdoc = require('swagger-jsdoc')
+
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'bCheck API',
+            version: '1.0.0',
+            description: 'bCheck API with nodejs + express',
+            contact: {
+                name: 'hanbee',
+                url: 'https://github.com/hanbee1005',
+                email: 'sonhanbi1002@gmail.com'
+            },
+        },
+        host: 'localhost:9090',
+        basePath: '/',
+    },
+    apis: ['./app/controllers/*.js']
+}
+
+const specs = swaggerJsdoc(options)
+
+module.exports = {
+    swaggerUi,
+    specs
+}
