60c65dd885b8eaa88f0d9f1bdb557916300c78e9
hanbee <sonhanbi1002@gmail.com>
2021-10-26 10:20:51 +0900

    book controller 생성 후 create 메소드 생성

---
+++ app/controllers/book.controller.js
@@ -0,0 +1,45 @@
+const db = require('../models')
+const Book = db.book
+
+const Op = db.Sequelize.Op
+
+// Find All Books (with title, owner_name)
+
+// Create and Save a new Book
+exports.create = (req, res) => {
+    // TODO: Validate request
+    // 1. isbn 확인
+    // 2. owner_email 확인
+
+    // Create a Book
+    const book = {
+        title: req.body.title,
+        link: req.body.link,
+        image: req.body.image,
+        author: req.body.author,
+        price: req.body.price,
+        discount: req.body.discount || '-',
+        publisher: req.body.publisher,
+        pubdate: req.body.pubdate,
+        isbn: req.body.isbn,
+        description: req.body.description,
+        owner_email: req.body.owner_email,
+        owner_name: req.body.owner_name,
+        del_yn: 'N',
+    }
+
+    // Save Book in the database
+    Book.create(book)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while creating the Book.'
+            })
+        })
+}
+
+// Update Book Owner
+
+// Delete Book
